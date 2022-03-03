986ed2b232e4e77a9a4b1ae78e02451239386759
superflem <alex.flem@hotmail.it>
2022-03-03 13:03:37 +0100

    aggiunti i filtri alla lista delle transizioni

--- API/typescript/listTransactions.ts
+++ API/typescript/listTransactions.ts
@@ -27,6 +27,49 @@ function listTransactions (req, response)
 
     client.eseguiList(invio, (err, res) => {
         //console.log(res["isTuttoOk"]);
+
+        const lista = JSON.parse(res["listaTransizioni"]);
+        //devo aggiungere +1 a tutti i giorni, perchè quando faccio la query al database, dà sempre un giorno in meno (non so perchè)
+        for (let i = 0; i < lista.length; i++)
+        {
+            const vecchiaData = lista[i].data.slice(0, 10); //prendo il mese il giorno e l'anno della vecchia data
+            let giorno = Number(vecchiaData.slice(8, 10));
+            let mese = Number(vecchiaData.slice(5, 7));
+            let anno = Number(vecchiaData.slice(0, 4));
+            
+            giorno = giorno + 1;
+            //controllo sul giorno
+            if (giorno == 32)
+            {
+                giorno = 1;
+                mese = mese+1;
+            }
+            if (giorno == 31 && (mese == 11 || mese == 4 || mese == 6 || mese == 9)) //mesi da 30 giorni
+            {
+                giorno = 1;
+                mese = mese+1;
+            }
+            //controllo sul mese
+            if (mese == 13)
+            {
+                mese = 1;
+                anno = anno+1;
+            }
+            //compongo la data (con gli zeri)
+            let nuovaData = anno+'-';
+            if (mese < 10)
+                nuovaData = nuovaData + '0'+mese+'-';
+            else
+                nuovaData = nuovaData +mese+'-';
+            if (giorno < 10)
+                nuovaData = nuovaData + '0'+giorno;
+            else
+                nuovaData = nuovaData +giorno;
+
+            lista[i].data = nuovaData;
+        }
+        res["listaTransizioni"] = JSON.stringify(lista);
+
         response.status(200).json(JSON.stringify(res)); //rimando al client
     });
 }
